7039f80fe1c973ec4daa5744c9ae951f64a7a2cc
yannie <ychernovskaya@gmail.com>
2023-02-14 16:00:38 +0700

    update main

--- Task2/src/main/java/ru/nsu/fit/yana/task2/Main.java
+++ Task2/src/main/java/ru/nsu/fit/yana/task2/Main.java
@@ -2,15 +2,33 @@ package ru.nsu.fit.yana.task2;
 
 
 import java.io.InputStream;
+import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        Parser parser = new Parser(args);
+        CommandLineParser parser = new CommandLineParser(args);
         parser.parse();
 
         InputStream in = parser.getInputStream();
         Context ctx = new Context();
+
+        try(Scanner scanner = new Scanner(in))
+        {
+            while(scanner.hasNextLine())
+            {
+                try
+                {
+                String[] arr = scanner.nextLine().split(" ");
+                Command command = CommandFactory.createCommand(arr[0]);
+                command.load(arr, ctx);
+                }
+                catch (Exception e)
+                {
+                    System.err.println(e.getMessage());;
+                }
+            }
+        }
     }
 }
