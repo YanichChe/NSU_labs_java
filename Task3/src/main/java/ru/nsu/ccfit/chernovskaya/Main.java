f02bed8b2b23118093592afd2908a244b7520536
yannie <ychernovskaya@gmail.com>
2023-03-22 12:02:17 +0700

    add final to StartMenu

--- Task3/src/main/java/ru/nsu/ccfit/chernovskaya/Main.java
+++ Task3/src/main/java/ru/nsu/ccfit/chernovskaya/Main.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
-            StartMenu startMenu = new StartMenu();
+            final StartMenu startMenu = new StartMenu();
         });
     }
 }
