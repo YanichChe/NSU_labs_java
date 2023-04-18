b029180e540428be2965edb186d0eee33451d333
Yana Сhernovskaya <55744073+YanichChe@users.noreply.github.com>
2023-04-18 23:22:35 +0700

    recreate Dealer.java

---
+++ Task4/src/main/java/ru/nsu/ccfit/chernovskaya/factory/runnablet_asks/Dealer.java
@@ -0,0 +1,34 @@
+package ru.nsu.ccfit.chernovskaya.factory.runnablet_asks;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import ru.nsu.ccfit.chernovskaya.factory.product.auto.Auto;
+import ru.nsu.ccfit.chernovskaya.factory.warehouse.Warehouse;
+
+@RequiredArgsConstructor
+@Log4j2
+public class Dealer implements Runnable{
+
+    private final Warehouse<Auto> autoWarehouse;
+
+    @Getter
+    @Setter
+    private int dealerDelay;
+
+    @SneakyThrows
+    @Override
+    public void run() {
+        while (true) {
+            Auto car = autoWarehouse.deliver();
+            log.info("Dealer bought : Auto<" + car.getID() + "> " +
+                    "(Body:<" + car.getBody().getID() + ">, " +
+                    "Motor:<" + car.getMotor().getID() + ">, " +
+                    "Accessory:<" + car.getAccessory().getID() + ">)");
+            Thread.sleep(dealerDelay);
+        }
+
+    }
+}
