30922b4ac06771fdc4aff4153ab8bbedb3bdfe68
Yana Сhernovskaya <55744073+YanichChe@users.noreply.github.com>
2023-04-18 23:29:11 +0700

    change logic in ThreadPool

--- Task4/src/main/java/ru/nsu/ccfit/chernovskaya/threadpool/ThreadPool.java
+++ Task4/src/main/java/ru/nsu/ccfit/chernovskaya/threadpool/ThreadPool.java
@@ -1,17 +1,13 @@
 package ru.nsu.ccfit.chernovskaya.threadpool;
 
 import lombok.extern.log4j.Log4j2;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import ru.nsu.ccfit.chernovskaya.product.Product;
 
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 @Log4j2
 public class ThreadPool{
+    private final List<PooledThread> threads = new LinkedList<>();
     private final List<Task> taskQueue = new LinkedList<>();
 
     public void addTask(Task task) {
@@ -21,13 +17,13 @@ public class ThreadPool{
         }
     }
 
-    public ThreadPool(int threadsCount) {
-        Set<PooledThread> availableThreads = new HashSet<>();
-        for (int i = 0; i < threadsCount; ++i) {
-            availableThreads.add(new PooledThread("Thread " + i, taskQueue));
-            log.info("Thread " + i + "was creating");
-        }
-        for (PooledThread availableThread : availableThreads) {
+    public void addThread(PooledThread taskThread) {
+        taskThread.setTaskQueue(taskQueue);
+        threads.add(taskThread);
+    }
+
+    public void start(){
+        for (PooledThread availableThread : threads) {
             availableThread.start();
             log.info("Thread " + availableThread.getName() +  " was creating");
         }
