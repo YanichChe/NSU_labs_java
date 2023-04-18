d98eaf648034be965f90a557385c26fdc19ea2c6
Yana Сhernovskaya <55744073+YanichChe@users.noreply.github.com>
2023-04-18 23:31:20 +0700

    change supplier logic

---
+++ Task4/src/main/java/ru/nsu/ccfit/chernovskaya/factory/runnablet_asks/Supplier.java
@@ -0,0 +1,40 @@
+package ru.nsu.ccfit.chernovskaya.factory.runnablet_asks;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+import ru.nsu.ccfit.chernovskaya.factory.product.Product;
+import ru.nsu.ccfit.chernovskaya.factory.warehouse.Warehouse;
+
+import java.lang.reflect.InvocationTargetException;
+
+@Log4j2
+@RequiredArgsConstructor
+public class Supplier<T extends Product> implements Runnable{
+
+    private final Warehouse<T> warehouse;
+    private final Class<T> productClass;
+
+    @Getter
+    @Setter
+    private int supplierDelay;
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()){
+            try {
+                Thread.sleep(supplierDelay);
+                T product = productClass.getDeclaredConstructor().newInstance();
+                warehouse.put(product);
+            } catch (InterruptedException e) {
+                log.info(Thread.currentThread().getName() + " was interrupted");
+                break;
+            }
+            catch (InvocationTargetException | InstantiationException | IllegalAccessException | NoSuchMethodException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
