4a34d7afaeffa8637e19fa3908f5ed6dd424d370
Yana Сhernovskaya <55744073+YanichChe@users.noreply.github.com>
2023-04-18 23:32:08 +0700

    create worker class

---
+++ Task4/src/main/java/ru/nsu/ccfit/chernovskaya/factory/worker/Worker.java
@@ -0,0 +1,19 @@
+package ru.nsu.ccfit.chernovskaya.factory.worker;
+
+import lombok.extern.log4j.Log4j2;
+import ru.nsu.ccfit.chernovskaya.threadpool.PooledThread;
+import ru.nsu.ccfit.chernovskaya.threadpool.Task;
+
+import java.util.List;
+
+@Log4j2
+public class Worker extends PooledThread {
+
+    public static int global_ID = 0;
+
+    public Worker() {
+        super.setID(global_ID);
+        global_ID++;
+        log.info("Worker:" + super.getID() + "was created");
+    }
+}
