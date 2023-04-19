ec156e5e2df99548a450bf420c7bd083c43922e4
Yana Сhernovskaya <55744073+YanichChe@users.noreply.github.com>
2023-04-19 22:52:13 +0700

    start WorkerThreadPool

---
+++ Task4/src/main/java/ru/nsu/ccfit/chernovskaya/factory/worker/WorkerThreadPool.java
@@ -0,0 +1,25 @@
+package ru.nsu.ccfit.chernovskaya.factory.worker;
+
+import lombok.extern.log4j.Log4j2;
+import ru.nsu.ccfit.chernovskaya.threadpool.Task;
+import ru.nsu.ccfit.chernovskaya.threadpool.ThreadPool;
+
+@Log4j2
+public class WorkerThreadPool {
+    private final ThreadPool threadPool = new ThreadPool();
+
+    public WorkerThreadPool(int workerCount) {
+        for (int i = 0; i < workerCount; i++) {
+            Worker worker = new Worker();
+            threadPool.addThread(worker);
+        }
+    }
+
+    public void addTask(Task task) {
+        threadPool.addTask(task);
+    }
+
+    public void startWork() {
+        threadPool.start();
+    }
+}
