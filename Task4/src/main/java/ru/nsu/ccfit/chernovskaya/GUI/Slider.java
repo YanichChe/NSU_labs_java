07e71e64a49c664bc488f6f8fb1ee50da1852a3b
Yana Сhernovskaya <55744073+YanichChe@users.noreply.github.com>
2023-04-23 19:38:38 +0700

    change bounds

--- Task4/src/main/java/ru/nsu/ccfit/chernovskaya/GUI/Slider.java
+++ Task4/src/main/java/ru/nsu/ccfit/chernovskaya/GUI/Slider.java
@@ -1,15 +1,10 @@
 package ru.nsu.ccfit.chernovskaya.GUI;
-
-import ru.nsu.ccfit.chernovskaya.factory.product.auto.Accessory;
-import ru.nsu.ccfit.chernovskaya.factory.product.auto.Body;
-import ru.nsu.ccfit.chernovskaya.factory.product.auto.Motor;
 import javax.swing.*;
-import javax.swing.event.ChangeListener;
 
 public class Slider extends JSlider{
     public final static int SEC = 1000;
-    public static final int FPS_MIN = 0;
-    public static final int FPS_MAX = 30;
+    public static final int FPS_MIN = 1;
+    public static final int FPS_MAX = 31;
     public static final int FPS_INIT = 15;
 
     public Slider(){
@@ -17,7 +12,7 @@ public class Slider extends JSlider{
         super(JSlider.HORIZONTAL,
                 FPS_MIN, FPS_MAX, FPS_INIT);
 
-        this.setMajorTickSpacing(10);
+        this.setMajorTickSpacing(5);
         this.setMinorTickSpacing(1);
         this.setPaintTicks(true);
         this.setPaintLabels(true);
