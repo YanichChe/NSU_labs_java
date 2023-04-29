a0b127ab226b8545032fba7d61297a5680a85eb8
Yana Сhernovskaya <55744073+YanichChe@users.noreply.github.com>
2023-04-29 12:48:51 +0700

    fix id name

--- Task4/src/main/java/ru/nsu/ccfit/chernovskaya/factory/product/Product.java
+++ Task4/src/main/java/ru/nsu/ccfit/chernovskaya/factory/product/Product.java
@@ -5,14 +5,14 @@ import lombok.Getter;
 @Getter
 public abstract class Product {
 
-    public static int global_ID = 0;
+    public static int globalID = 0;
     private int ID;
     private final Object monitor = new Object();
 
     public Product(){
         synchronized (monitor){
-            ID = global_ID;
-            global_ID++;
+            ID = globalID;
+            globalID++;
         }
     }
 }
