30a9ec81aa82c627d2ef995c9c19071ae245c4ed
Yana Сhernovskaya <55744073+YanichChe@users.noreply.github.com>
2023-06-26 15:14:20 +0700

    change parser

--- Task2/src/main/java/ru/nsu/fit/yana/task2/Main.java
+++ Task2/src/main/java/ru/nsu/fit/yana/task2/Main.java
@@ -1,21 +1,44 @@
 package ru.nsu.fit.yana.task2;
 
+import com.google.devtools.common.options.OptionsParser;
 import lombok.extern.log4j.Log4j2;
-import ru.nsu.fit.yana.task2.exceptions.UndefinedFileNameException;
+import ru.nsu.fit.yana.task2.commandLineParser.ConsoleArguments;
+import ru.nsu.fit.yana.task2.commandLineParser.ConsoleArgumentsData;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collections;
 import java.util.Scanner;
 
 @Log4j2
 public class Main {
-    public static void main(String[] args) throws UndefinedFileNameException {
+    public static void main(String[] args) throws IOException {
         log.info("Initialize Calc application");
         log.info("Parsing command line");
 
-        CommandLineParser parser = new CommandLineParser(args);
-        parser.parse();
+        OptionsParser parser = OptionsParser.newOptionsParser(ConsoleArgumentsData.class);
+        parser.parseAndExitUponError(args);
+
+        ConsoleArguments options = new ConsoleArguments(parser.getOptions(ConsoleArgumentsData.class));
+        System.out.println(parser.describeOptions(Collections.<String, String>emptyMap(), OptionsParser.HelpVerbosity.LONG));
+
+        InputStream in;
+
+        if (options.getFile().isEmpty()) {
+            in = System.in;
+        } else {
+            try {
+                in = new FileInputStream(options.getFile());
+            }
+            catch (FileNotFoundException e) {
+                log.error(e.getMessage());
+                return;
+            }
+        }
 
         log.info("Create context");
-        InputStream in = parser.getInputStream();
         Context ctx = new Context();
 
         log.info("load Calc");
@@ -38,5 +61,6 @@ public class Main {
             }
         }
         log.info("END");
+        in.close();
     }
 }
