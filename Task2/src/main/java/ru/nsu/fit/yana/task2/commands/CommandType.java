a5be6d0fe2aaaca4c3f70cf5666e66d37f137d99
Yana Сhernovskaya <55744073+YanichChe@users.noreply.github.com>
2023-06-26 15:54:00 +0700

    update

---
+++ Task2/src/main/java/ru/nsu/fit/yana/task2/commands/CommandType.java
@@ -0,0 +1,14 @@
+package ru.nsu.fit.yana.task2.commands;
+
+public enum CommandType {
+    DEFINE,
+    DIVISION,
+    MULTIPLICATION,
+    POP,
+    PRINT,
+    PUSH,
+    SQRT,
+    SUBTRACTION,
+    SUM
+}
+
